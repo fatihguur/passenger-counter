6df481a78a140fc95dd3cf75ac3eaebfc9470429
Fatih Gur <fatih.gur26@hotmail.com>
2022-11-01 20:11:07 +0300

    Passenger Counter Complete

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ let myAge = 24;
 let humanDogRatio = 7;
 
 
+
 let count = 0;
+let countEl = document.getElementById("count-el");
+
+let welcomeEl = document.getElementById("welcome-el");
+let myName = "Fatih";
+let message = "welcomes you!!";
+
+let saveEl = document.getElementById("save-el");
+
+welcomeEl.innerText = myName+ " " + message;
+
+welcomeEl.innerText = welcomeEl.innerText + "👋";
+
 
 
 
 function increment() {
-    document.getElementById("count-el").innerText++;
+    count++;
+    countEl.textContent = count;
+}
+
+function save() {
+    saveEl.textContent = saveEl.innerText + count + " " +" - ";
+    count = 0;
+    console.log(count);
 }
